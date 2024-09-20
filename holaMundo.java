cc34e93d629f88823245537e08336c53d2cb0442
alfonss90@gmail.com <alfonss90@gmail.com>
2024-09-20 18:34:49 +0200

    1.1

--- holaMundo.java
+++ holaMundo.java
@@ -1,6 +1,6 @@
 public class holaMundo{
   public static void main (String[] args){
   System.out.print("Hola mundo");
-    System.out.print("Esto va a petar o no");
+    System.out.print("Esto va a petar o si");
   }
 }
