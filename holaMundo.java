65206a57b8ef85312f70320d4563fde334479114
alfonss90@gmail.com <alfonss90@gmail.com>
2024-09-26 18:43:20 +0200

    programa pruebas comomedalagana.txt

--- holaMundo.java
+++ holaMundo.java
@@ -2,5 +2,6 @@ public class holaMundo{
   public static void main (String[] args){
   System.out.print("Hola mundo");
     System.out.print("Esto va a petar o si");
+System.out.print("Junior vete ya");
   }
 }
